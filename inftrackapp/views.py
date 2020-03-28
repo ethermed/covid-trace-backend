832605f10a8d88856c259777279c04a14f5471ea
Matt Walker <xdmwalkerx@gmail.com>
2020-03-28 17:45:29 -0400

    Added urls.py routes and view functions for /status. One will allow the setting of the status and the other returns the person with the matching unique_id specified in the request

--- inftrackapp/views.py
+++ inftrackapp/views.py
@@ -39,7 +39,7 @@ def show_all_people(request):
 
 # v1/people?role=doctor
 # v1/people?role=doctor,nurse,patient
-def show_people_by_role(request,*args,**kwargs):
+def show_people_by_role(self, request, *args, **kwargs):
     #comma delimited roles or one role
     role_value = kwargs["role"]
     #handling one role
@@ -64,9 +64,9 @@ def show_people_by_role(request,*args,**kwargs):
         people_list.append(person_dict)
     return api_json.response_success_with_list(people_list)
 
-# v1/people/?status=ok
-# v1/people/?status=at-risk,being-tested
-def show_people_by_status(request,*args,**kwargs):
+# v1/people?status=ok
+# v1/people?status=at-risk,being-tested
+def show_people_by_status(self, request, *args, **kwargs):
     #comma delimited statuses or one statuses
     status_value = kwargs["status"]
     #handling one status
@@ -90,3 +90,46 @@ def show_people_by_status(request,*args,**kwargs):
         }
         people_list.append(person_dict)
     return api_json.response_success_with_list(people_list)
+
+# v1/status?id=123
+def show_person_by_id(self, request, *args, **kwargs):
+    id = kwargs["id"]
+    trackable_people = models.TrackablePerson.objects.filter(unique_id=id)
+    for person in trackable_people:
+        person_dict = {
+            "firstname": person.firstname,
+            "lastname": person.lastname,
+            "id": person.unique_id,
+            "role": person.role,
+            "status": person.status,
+            "phone": person.phone,
+            "email": person.email
+        }
+        people_list.append(person_dict)
+    return api_json.response_success_with_list(people_list)
+
+# v1/status?id=123&status=ok
+def update_status(self, request, *args, **kwargs):
+    #get the user with id
+    updated_persons = models.TrackablePerson.objects.filter(unique_id=kwargs["id"])
+    updated_person = updated_persons[0]
+
+    #fetching new status value
+    status_update = kwargs["status"]
+
+    #checking to see if the new status value is a valid status
+    if status_update in STATUS_CHOICES:
+        setattr(updated_person,"status",status_update)
+
+    updated_person.save()
+
+    response_dict = {
+        "firstname": updated_person.firstname,
+        "lastname": updated_person.lastname,
+        "id": updated_person.unique_id,
+        "role": updated_person.role,
+        "status": updated_person.status,
+        "phone": updated_person.phone,
+        "email": updated_person.email
+    }
+    return api_json.response_success_with_dict(response_dict)
