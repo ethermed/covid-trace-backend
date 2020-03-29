5db56967382f2d618c738972e26261958ecf3074
Matt Walker <xdmwalkerx@gmail.com>
2020-03-29 15:40:22 -0400

    added swagger

--- inftrack/settings.py
+++ inftrack/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inftrackapp'
+    'inftrackapp',
+    'rest_framework_swagger'
 ]
 
 MIDDLEWARE = [
