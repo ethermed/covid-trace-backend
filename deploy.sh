811f6f51052fd854f876d5557ab1f07398f17142
msuch <such.michal@gmail.com>
2020-03-29 13:37:45 -0400

    Fixing deploy

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 #   kill $DJANGOPID
 # fi
 
-ps -aux | grep 'runserver 8080' | awk '{print $2}' | xargs kill -9
+ps -aux | grep 'runserver 0.0.0.0:8080' | awk '{print $2}' | xargs kill -9
 
 git pull
 
-nohup python manage.py runserver 8080 > api.log 2>&1 &
+nohup python manage.py runserver 0.0.0.0:8080 > api.log 2>&1 &
 
 # export DJANGOPID=$(echo $!)
